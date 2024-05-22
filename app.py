5d4d1e02f6c55eda22e0404ce3748d239a0011a8
TranHieu170295 <hieu170295@gmail.com>
2024-05-22 21:04:45 +0700

    Update

--- app.py
+++ app.py
@@ -1,62 +1,48 @@
 from flask import Flask, render_template, request
 from bs4 import BeautifulSoup
 import requests
+from urllib.parse import urljoin
 
 app = Flask(__name__)
 
-def scrapedata1(url1):
-    response = requests.get(url1)
-    soup = BeautifulSoup(response.content, "html.parser")
-
+def scrapedata(url):
     conferences = []
+    while url:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
 
-    conference_list = soup.find("div", {"id": "eventList"}).find_all("li")
+        conference_list = soup.find("div", {"id": "eventList"}).find_all("li")
 
-    for item in conference_list:
-        if len(item.contents) > 1:
-            time_info = item.contents[0].strip()
-            conference_name = item.find("a").text.strip()
-            location_info = item.contents[-1].strip()
-            conference_link = item.find("a").get("href")
+        for item in conference_list:
+            if len(item.contents) > 1:
+                time_info = item.contents[0].strip()
+                conference_name = item.find("a").text.strip()
+                location_info = item.contents[-1].strip()
+                conference_link = item.find("a").get("href")
 
-            conferences.append({
-                'time': time_info,
-                'name': conference_name,
-                'location': location_info,
-                'link': conference_link
-            })
+                conferences.append({
+                    'time': time_info,
+                    'name': conference_name,
+                    'location': location_info,
+                    'link': conference_link
+                })
+        pagination = soup.find("ul", class_="pagination")
+        if pagination:
+            next_page = pagination.find("a", rel="next")
+            if next_page:
+                url = urljoin(url, next_page.get("href"))
+            else:
+                url = None
+        else:
+            url = None
     return conferences
-def scrapedata2(url2):
-    response = requests.get(url2)
-    soup = BeautifulSoup(response.content, "html.parser")
-    
-    # Tạo một danh sách trống để lưu trữ thông tin đã in ra
-    conferences = []
-    # Tìm tất cả các thẻ <li> chứa thông tin về hội nghị
-    for table in soup.find_all('table'):
-        for row in table.find_all('tr', class_='row-2 even'):
 
-            time_info = row.find('strong').get_text()
-            location_info = row.find('td').get_text().split(':')[1][1:].split('[')[0]
-            conference_name = row.find('a').get_text()
-            conference_link = row.find('a')['href']
-            conferences.append({
-                'time': time_info,
-                'name': conference_name,
-                'location': location_info,
-                'link': conference_link
-            })
-    return conferences
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    conferences_data = None
     if request.method == 'POST':
-        url_type = request.form['url_type']
         url_data = request.form['url']
-        if url_type == 'url1':
-            conferences_data = scrapedata1(url_data)
-        elif url_type == 'url2':
-            conferences_data = scrapedata2(url_data)
-        return render_template('index.html', conferences=conferences_data)
-    return render_template('index.html', conferences=None)
+        conferences_data = scrapedata(url_data)
+    return render_template('index.html', conferences=conferences_data)
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5500)
